fd269c6518eee01aa9cd051254e26d8a63b8a0c0
Madan Golay Tamang <madantamang939@gmail.com>
2025-09-01 18:49:35 +0545

    scroll to top added

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,36 @@
+import { Route, Routes, useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import Tour from "./pages/Tours";
+import Package1 from "./pages/ToursPackage/Package1";
+import Package2 from "./pages/ToursPackage/Package2";
 
-import { Route, Routes } from 'react-router-dom'
-import Tour from './pages/Tours'
-import Package1 from './pages/ToursPackage/Package1'
-import Package2 from './pages/ToursPackage/Package2'
+// ScrollToTop Component
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 
 function App() {
   return (
     <>
+      {/* Place ScrollToTop here */}
+      <ScrollToTop />
+
       <Routes>
-        <Route path='/' element={<Tour />} />
-        <Route path='/dubai-3-nights-4-days-package-explore-dubai' element={<Package1/>}/>
-        <Route path='/dubai-Helicopter-view' element={<Package2/>}/>
+        <Route path="/" element={<Tour />} />
+        <Route
+          path="/dubai-3-nights-4-days-package-explore-dubai"
+          element={<Package1 />}
+        />
+        <Route path="/dubai-Helicopter-view" element={<Package2 />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
